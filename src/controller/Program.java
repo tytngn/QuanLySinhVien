f1d1fad90cfb770a93696d5aa6fbf346732a5a58
nguyenthituyetngan <nguyenthituyetngan@172-14-56-130.lightspeed.jcvlfl.sbcglobal.net>
2024-01-12 12:35:36 +0700

    init project

---
+++ src/controller/Program.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.DM_MonHoc;
+
+/**
+ *
+ * @author nguyenthituyetngan
+ */
+public class Program {
+    public static void main(String[] args) {
+        DM_MonHoc.getInstance();
+    }
+}
